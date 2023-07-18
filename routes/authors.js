38df945f4c55aac63d4f510726396d3aa52e3453
Sky Zhao <zhaosky12187@gmail.com>
2023-07-18 17:45:37 -0400

    error message

---
+++ routes/authors.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const router = express.Router();
+const Author = require("../models/author");
+
+// All Authors Route
+
+router.get("/", (req, res) => {
+  res.render("authors/index");
+});
+
+// New Author Route
+router.get("/new", (req, res) => {
+  res.render("authors/new", { author: new Author() });
+});
+
+// Create Author Route
+router.post("/", (req, res) => {
+  const author = new Author({
+    name: req.body.name,
+  });
+  author.save((err, newAuthor) => {
+    if (err) {
+      res.render("authors/new", {
+        author: author,
+        errorMessage: "Error creating author",
+      });
+    } else {
+      // res.redirect("authors/${newAuthor.id");
+      res.redirect(`authors`);
+    }
+  });
+  // res.send(req.body.name);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,22 +5,26 @@ if (process.env.NODE_ENV !== "production") {
 const express = require("express");
 const app = express();
 const expressLayouts = require("express-ejs-layouts");
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
+const authorRouter = require("./routes/authors");
 
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout"); //header & footer
 app.use(expressLayouts);
 app.use(express.static("public")); //javeScript, style sheet
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
 
 const mongoose = require("mongoose");
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASE_URL);
 
 const db = mongoose.connection;
 db.on("error", (error) => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
 app.use("/", indexRouter);
+app.use("/authors", authorRouter);
 
 app.listen(process.env.PORT || 3000);
