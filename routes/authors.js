410e796681e7f993a9ac5a434cc2072f9ccf2b1d
Sky Zhao <zhaosky12187@gmail.com>
2023-07-19 17:07:45 -0400

    initial author router

--- routes/authors.js
+++ routes/authors.js
@@ -4,8 +4,18 @@ const Author = require("../models/author");
 
 // All Authors Route
 
-router.get("/", (req, res) => {
-  res.render("authors/index");
+router.get("/", async (req, res) => {
+  let searchOptions = {};
+  if (req.query.name != null && req.query.name !== "") {
+    searchOptions.name = new RegExp(req.query.name, "i");
+  }
+  try {
+    // const authors = await Author.find(); // get all authors
+    const authors = await Author.find(searchOptions); // get author by a condition
+    res.render("authors/index", { authors: authors, searchOptions: req.query });
+  } catch {
+    res.redirect("/");
+  }
 });
 
 // New Author Route
@@ -14,22 +24,37 @@ router.get("/new", (req, res) => {
 });
 
 // Create Author Route
-router.post("/", (req, res) => {
+// router.post("/", (req, res) => {
+//   const author = new Author({
+//     name: req.body.name,
+//   });
+//   author.save((err, newAuthor) => {
+//     if (err) {
+//       res.render("authors/new", {
+//         author: author,
+//         errorMessage: "Error creating author",
+//       });
+//     } else {
+//       // res.redirect("authors/${newAuthor.id");
+//       res.redirect(`authors`);
+//     }
+//   });
+//   // res.send(req.body.name);
+// });
+
+router.post("/", async (req, res) => {
   const author = new Author({
     name: req.body.name,
   });
-  author.save((err, newAuthor) => {
-    if (err) {
-      res.render("authors/new", {
-        author: author,
-        errorMessage: "Error creating author",
-      });
-    } else {
-      // res.redirect("authors/${newAuthor.id");
-      res.redirect(`authors`);
-    }
-  });
-  // res.send(req.body.name);
+  try {
+    const newAuthor = await author.save();
+    res.redirect(`authors`);
+  } catch {
+    res.render("authors/new", {
+      author: author,
+      errorMessage: "Error creating author",
+    });
+  }
 });
 
 module.exports = router;
